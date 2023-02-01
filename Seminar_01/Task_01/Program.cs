cc19b62dcfbe185e57c7ea89f938ae039f6b0813
Karina <kamironova09@gmail.com>
2023-02-01 20:16:43 +0300

    Task

---
+++ Seminar_01/Task_01/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.Write("a");
+}
+
+else
+{
+    Console.Write("b");
+}
