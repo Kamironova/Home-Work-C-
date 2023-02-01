a30dd054202dd4021a8b1e74da9d97485297d75c
Karina <kamironova09@gmail.com>
2023-02-01 21:24:24 +0300

    Task 2

--- Seminar_01/Task_02/Program.cs
+++ Seminar_01/Task_02/Program.cs
@@ -1,1 +1,43 @@
-﻿
+﻿Console.Clear();
+
+Console.WriteLine("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = 0;
+
+if (a > b)
+{
+    if (a > c)
+        {
+            Console.Write("max = ");
+            Console.Write(a);
+        }
+
+        
+    else
+    {
+        Console.Write("max = ");
+        Console.Write(c);
+    }
+}
+else
+{
+    if (b > c)
+    {
+        Console.Write("max = ");
+        Console.Write(b);
+    }
+    else
+    {
+        Console.Write("max = ");
+        Console.Write(c);
+
+
+    }
+}
