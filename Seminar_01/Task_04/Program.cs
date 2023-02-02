013701520758f58eddd2001dbad85ac86058ddd3
Karina <kamironova09@gmail.com>
2023-02-02 14:42:12 +0300

    Task 4

---
+++ Seminar_01/Task_04/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+System.Console.WriteLine("--> ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+int i = 2;
+
+while (i <= a)
+{
+    if (i % 2 == 0)
+    {
+        System.Console.Write(i + " ");
+    }
+    
+ i++;
+};
