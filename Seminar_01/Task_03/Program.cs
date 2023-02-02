79334781bd157a226881d044c0e29ab5f1d267de
Karina <kamironova09@gmail.com>
2023-02-02 13:52:37 +0300

    Task 0

---
+++ Seminar_01/Task_03/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("--> ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 = 0)
+{
+    Console.Write("Число является четным");
+}
+
+else
+{
+    Console.Write("Число не является четным");
+}
