6654a3bff258bcbc26a7bc8a3d078ee699d408f9
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-09 14:32:52 +0300

    Task 01

---
+++ Seminar_02/Task_01/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+
+System.Console.Write("Введите трехзначное число --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = a % 10;
+int c = a % 100 / 10;
+
+
+System.Console.WriteLine(c);
