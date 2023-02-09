64abb0e53c43847baa385c56472751f0cfaa3089
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-09 15:45:41 +0300

    tasl-02

---
+++ Seminar_02/Task_02/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+
+
+System.Console.Write("Введите число --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a < 99)
+{
+    System.Console.WriteLine("Третьей цифры нет");
+    
+}
+else
+{
+    while (a > 999)
+    {
+        a = a / 10;
+    }
+
+    int b = a % 10;
+    System.Console.WriteLine(b);
+}
+
