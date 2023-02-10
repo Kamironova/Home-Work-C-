42b3e7f699e9bbc58452c5e8c225497780316710
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-10 12:06:56 +0300

    Sem 1, task 3

---
+++ Seminar_03/Task_03/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+System.Console.Write("-->");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 0; i <= n; i++)
+{
+    System.Console.Write($" {i * i * i} "); // подумать над красивым форматированием
+}
+System.Console.WriteLine();
