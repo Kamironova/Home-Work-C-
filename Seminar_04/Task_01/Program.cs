192e01e6c60c5727f9d0c7750f91e17aa704d29c
Карина Миронова <kamironova@192.168.1.9>
2023-02-12 19:34:47 +0300

    HW t1

---
+++ Seminar_04/Task_01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход числа A и B 
+// и возводит число A в натуральную степень B.
+
+Console.Clear();
+
+System.Console.Write("Введите число A --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+System.Console.Write("введите число B --> ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int j = 1;
+int i = 1;
+while (i <= b)
+{
+    j = j * a;
+    i++;
+}
+
+System.Console.WriteLine(j);
+
+//${a}", "{b}" --> {j}"
