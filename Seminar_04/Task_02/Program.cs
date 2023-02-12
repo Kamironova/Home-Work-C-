a3333a8d7a5a6375e70d9e2187bcb676dc7d9a39
Карина Миронова <kamironova@192.168.1.9>
2023-02-12 21:45:40 +0300

    sem 04, t 2

---
+++ Seminar_04/Task_02/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и 
+// выдает сумму цифр в этом числе
+
+Console.Clear();
+
+Console.Write("--> ");
+
+int a = Convert.ToInt32(Console.ReadLine());
+int s = 0;
+while (a > 0)
+{ 
+    s = s + a % 10;
+    a = a / 10 ;
+ 
+}
+Console.WriteLine($"Сумма цифр равна {s}");
