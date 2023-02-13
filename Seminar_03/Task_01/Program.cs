0980538096cd9db7e1b8a950b0a1794c4ddf795f
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-13 11:38:59 +0300

    Палиндром

--- Seminar_03/Task_01/Program.cs
+++ Seminar_03/Task_01/Program.cs
@@ -5,5 +5,24 @@
 
 Console.Clear();
 
-System.Console.WriteLine("Введите пятизначное число --> ");
+System.Console.Write("Введите пятизначное число --> ");
 
+string? number = Console.ReadLine();
+
+void CheckNumber(string number)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        System.Console.WriteLine($"Число {number} является палиндромом");
+    }
+    else
+    {
+        System.Console.WriteLine($"Число {number} не является палиндромом");
+    }
+}
+
+if (number!.Length == 5)
+{
+    CheckNumber(number);
+}
+else System.Console.WriteLine("Введите пятизначное число --> ");
