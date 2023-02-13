03a49c231aa30237c70c226243dfe026dbc73ac1
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-13 12:53:19 +0300

    d

---
+++ Seminar_04/Task_03/Program.cs
@@ -0,0 +1,18 @@
+﻿// Объявить массив из 8 элементов и заполнить его случайным значениями
+
+Console.Clear();
+
+int[] array = new int[8];
+
+Random rnd = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(10);
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    System.Console.Write($"[{array[i] + "]"}");
+}
+System.Console.WriteLine();
