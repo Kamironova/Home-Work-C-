096f9b31c040cd5d34e80c6b63f01e1ef8b0537c
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-15 15:38:07 +0300

    Task 03

---
+++ Seminar_05/Task_03/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным элементами массива.
+
+Console.Clear();
+
+// string str = Console.ReadLine();
+// string[] arr = str.Split();
+// double[] arrint = new double[arr.Length];
+
+// for (double i = 0; i < arr.Length; i++)
+// {
+//     double[] = Convert.ToDouble(arr[i]);
+// }
+
+System.Console.Write("Введите количество элементов массива --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+double[] array = new double[a];
+
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(0, 99);
+    System.Console.Write($"[{array[i] + "]"}");
+}
+System.Console.WriteLine();
+
+double min = array[0];
+double max = array[0];
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min)
+    {
+        min = array[i];
+
+    }
+    if (array[i] > max)
+    {
+        max = array[i];
+    }
+}
+System.Console.WriteLine($"Min -> {min}");
+System.Console.WriteLine($"Max -> {max}");
+
+
+System.Console.WriteLine($"Разница -> {max - min}");
