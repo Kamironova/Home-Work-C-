dfcf04454c3dcf68a7b3047e664758444d5b1e85
Карина Миронова <kamironova@MacBook-Air-Karina.local>
2023-02-15 14:23:06 +0300

    Task 02

---
+++ Seminar_05/Task_02/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задайте одномерный массив, заполненный случайными числами.
+// Найдите сумму элементов стоящих на нечетных позициях.
+
+Console.Clear();
+
+System.Console.Write("Введите количество элементов массива --> ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[a];
+
+Random rnd = new Random();
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rnd.Next(-50, 50);
+    System.Console.Write($"[{array[i] +"]"}");
+}
+System.Console.WriteLine();
+
+int sum = 0;
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i % 2 != 0)
+    {
+        sum += array[i];
+
+    }
+}
+
+System.Console.WriteLine(sum);
